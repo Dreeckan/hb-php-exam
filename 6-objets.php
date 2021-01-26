4ef17ecad49076bfd66948d6ea94d12a2d82b9cb
MarieF118 <72809324+MarieF118@users.noreply.github.com>
2021-01-26 14:58:47 +0100

    exo 6 : classes et objets

--- 6-objets.php
+++ 6-objets.php
@@ -2,6 +2,15 @@
 spl_autoload_register(function ($class) {
     // Dé-commenter la ligne ci-dessous si vous utilisez un Mac avec MAMP
     // $class = str_replace($class, '\\', '/');
-    require_once "classes/$class.php";
+    require_once "classes/Game/$class.php";
 });
 
+$videoGame = new VideoGame();
+$videoGame->setName('Among Us')->setPrice(4);
+
+$boardGame = new VideoGame();
+$boardGame->setName('Blanc manger coco')->setPrice(29.9);
+
+echo $videoGame->getName() . ' ' . $videoGame->getPrice() . ' €';
+echo '</br>';
+echo $boardGame->getName() . ' ' . $boardGame->getPrice() . ' €';
---
+++ classes/Game/AbstractGame.php
@@ -0,0 +1,67 @@
+<?php
+
+// Une classe abstraite est une sorte de modèle sur lequel sur base les futures classes filles, on ne peut pas créer d'objet à partir de celle-ci, elle reste le plan de fabrication qu'on viendra surcharger avec d'autres propriétés ou méthodes des classes filles
+
+abstract class AbstractGame
+{
+    /**
+     * @var float
+     */
+    protected $price;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+
+    // met à jour le contenu de la propriété `price` à partir de la variable `$price`
+
+    /**
+     * @param float $price
+     *
+     * @return AbstractGame
+     */
+
+    public function setPrice(float $price): AbstractGame
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    // renvoie le contenu de la propriété `price`
+
+    /**
+     * @return float
+     */
+    public function getPrice():float
+    {
+        return $this->price;
+    }
+
+    
+
+    // met à jour le contenu de la propriété `name` à partir de la variable `$name`
+
+    /**
+     * @param string $name
+     *
+     * @return AbstractGame
+     */
+
+    public function setName(string $name): AbstractGame
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    // renvoie le contenu de la propriété `name`
+
+    /**
+     * @return string
+     */
+    public function getName():string
+    {
+        return $this->name;
+    }
+}
