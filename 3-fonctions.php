f939b4670e513297cf54f333de0a98ae050a7500
Alice1360 <72809385+Alice1360@users.noreply.github.com>
2021-01-26 13:37:11 +0100

    exo3-Alice

--- 3-fonctions.php
+++ 3-fonctions.php
@@ -1,1 +1,23 @@
 <?php
+
+function fonctionAffine(int $n): int
+{
+    if ($n > 5) {
+        return 4*$n + 6;
+    } else {
+        return 3*$n -2;
+    }
+};
+
+var_dump(fonctionAffine(1));
+var_dump(fonctionAffine(10));
+var_dump(fonctionAffine(5));
+
+function suiteNPlusUn(int $n): int
+{
+    return $n * ($n + 1)/2;
+};
+
+var_dump(suiteNPlusUn(4));
+var_dump(suiteNPlusUn(5));
+var_dump(suiteNPlusUn(2));
