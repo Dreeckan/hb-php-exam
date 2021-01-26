bd5ff0cc5ef710706e15b9f6882a4291e52e3c6b
Adam FARES <dadi71410@outlook.fr>
2021-01-26 13:33:18 +0100

    exercice-1-terminé-Adam

--- 1-variables.php
+++ 1-variables.php
@@ -1,1 +1,25 @@
 <?php
+
+
+// // La fonction array permet de créer un array
+// $objects = array ("test", 42, 0.0);
+
+// array_push($objects, "ajout");
+// array_push($objects["index"] = "ajout2");
+
+// var_dump($objects)
+
+
+// On crée notre array $coordonnees
+$objects = array (
+    1 => 'test',
+    2 => 42,
+    3 => 0.0
+);
+$objects[] =  "ajout";
+$objects["index"] = "ajout2";
+
+echo $objects["index"];
+
+
+?>
