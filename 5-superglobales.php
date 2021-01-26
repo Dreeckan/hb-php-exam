11c8c772cabc89c4df0957e67d4eb871e992e65b
MarieF118 <72809324+MarieF118@users.noreply.github.com>
2021-01-26 14:26:12 +0100

    exo5 : superglobales

--- 5-superglobales.php
+++ 5-superglobales.php
@@ -1,13 +1,48 @@
 <?php
+session_start();
+
+if (empty($_GET['price'])) {
+    header('Location: 1-html.php');
+}
+    $price = $_GET['price'];
+    $_SESSION['price'] = $price;
+
+    $errors = [];
+
+    if (isset($_POST['gift'])) {
+        if (empty($_POST['gift'])) {
+            $errors[] = "Veuillez saisir un don";
+        }
+    }
+
+    if (isset($_POST['email'])) {
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "Veuillez entrer un email valide";
+        }
+    }
+
+    if (isset($_POST['email']) && isset($_POST['gift']) && empty($errors)) {
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['sum'] = $_POST['gift'] + $price;
+        var_dump($_SESSION);
+    }
+
+    foreach ($errors as $error) {
+        echo $error.' <br />';
+    }
+
 ?>
 
-<form action="" method="post">
+<form action="5-superglobales.php?price=<?= $price ?>" method="post">
     <div>
         <label for="gift">Ajouter un don</label>
-        <input type="number" id="gift">
+        <input type="number" name="gift" id="gift">
     </div>
     <div>
         <label for="email">Un champ email</label>
-        <input type="email" id="email">
+        <input type="email" name="email" id="email">
+    </div>
+    <div>
+        <input type="submit" value="Valider">
     </div>
 </form>
