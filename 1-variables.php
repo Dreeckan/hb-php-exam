f0661ac97f4b9be9b90479e7b03e92e8711de861
Elwilya <69197349+Elwilya@users.noreply.github.com>
2021-01-26 15:05:25 +0100

    Commit exam 1-variables.php

--- 1-variables.php
+++ 1-variables.php
@@ -1,1 +1,23 @@
 <?php
+// Définition de l'objet comme un tableau et entrée des différentes valeurs.
+$objects = [
+    ['name' => 'test'],
+    ['name' => 42],
+    ['name' => 0.0],
+];
+
+
+$arr['name'] = 'ajout';
+?>
+
+<table>
+    <?php
+        foreach ($objects as $object) { //Pour chaque objet, on met une ligne.
+            ?>        
+        <tr>
+            <td><?php echo $object['name']; ?></td>
+        </tr>
+    <?php
+        }
+    ?>
+</table>
