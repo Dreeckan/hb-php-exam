895dd8f2d3f111580dfae5dd5ce80316b4af4b67
vincentvdt <vincent.vidot3@gmail.com>
2021-01-26 14:08:19 +0100

    vincent exam 4

--- 4-html.php
+++ 4-html.php
@@ -2,23 +2,51 @@
 
 $games = [
     [
-        'name'  => 'Hadès',
+        'name' => 'Hadès',
         'price' => 45,
     ],
     [
-        'name'  => 'Cyberpubnk 2077 - édition avec carte graphique',
+        'name' => 'Cyberpubnk 2077 - édition avec carte graphique',
         'price' => 850,
     ],
     [
-        'name'  => 'Disco Elysium',
+        'name' => 'Disco Elysium',
         'price' => 55,
     ],
     [
-        'name'  => 'Exo One',
+        'name' => 'Exo One',
         'price' => 25,
     ],
     [
-        'name'  => 'Among Us',
+        'name' => 'Among Us',
         'price' => 4,
     ],
 ];
+?>
+
+<table>
+    <thead>
+    <tr>
+        <td>Nom des jeux</td>
+        <td>Prix</td>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+
+
+    foreach ($games as $game) {
+        echo '
+            <>
+                <td><a href="5-superglobales.php?price=' . $game['price'] . '">' . $game['name'] . '</a></td>';
+
+        if ($game['price'] < 50) {
+            echo '<td>' . number_format($game['price'], 2, ',', ' ') . '€ pas cher !</td></tr>';
+        } else {
+            echo '<td>' . number_format($game['price'], 2, ',', ' ') . '€</td></tr>';
+        }
+
+    }
+    ?>
+    </tbody>
+</table>
