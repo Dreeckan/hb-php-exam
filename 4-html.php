8b484c47391ed2cc36d24befb05a9c68ae0aaf8b
Rémi Jarjat <remi@dreeckan.net>
2021-01-26 15:16:09 +0100

    exercice 4

--- 4-html.php
+++ 4-html.php
@@ -22,3 +22,27 @@ $games = [
         'price' => 4,
     ],
 ];
+?>
+<table>
+    <tr>
+        <td>Nom du jeu</td>
+        <td>Prix</td>
+    </tr>
+    <?php foreach ($games as $game) {?>
+        <tr>
+            <td>
+                <a href="5-superglobales.php?price=<?= $game['price']; ?>">
+                    <?= $game['name']; ?>
+                </a>
+            </td>
+            <td>
+                <?= number_format($game['price'], 2, ',', ' '); ?>€
+                <?php
+                    if ($game['price'] <= 50) {
+                        echo '(pas chère !)';
+                    }
+                ?>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
