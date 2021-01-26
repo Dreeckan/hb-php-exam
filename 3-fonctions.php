1c6ef48088b63bc4fa6a48be00a6c919d6c1a186
geoffrey <biggeo22@hotmail.fr>
2021-01-26 14:54:57 +0100

    fin de l'exercice 3

--- 3-fonctions.php
+++ 3-fonctions.php
@@ -1,1 +1,17 @@
 <?php
+function fonctionAffine(int $n): int
+{
+    if ($n > 5) {
+        return 4 * $n + 6;
+    } elseif ($n <= 5) {
+        return 3 * $n - 2;
+    }
+};
+
+function suiteNPlusUn(int $n): int
+{
+    return $n * ($n + 1) / 2;
+};
+
+var_dump(fonctionAffine(8), fonctionAffine(1), fonctionAffine(6));
+var_dump(suiteNPlusUn(12), suiteNPlusUn(5), suiteNPlusUn(6));
