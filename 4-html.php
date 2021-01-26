d817bc59cfbd29eb3ec77f6bc41d9106c475689b
Larazzia <sigismeau.clement@gmail.com>
2021-01-26 15:15:01 +0100

    fin exo 4

--- 4-html.php
+++ 4-html.php
@@ -22,3 +22,41 @@ $games = [
         'price' => 4,
     ],
 ];
+
+
+?>
+
+
+<table>
+    <thead>
+        <tr>
+            <th>nom du jeu</th>
+            <th>prix</th>
+
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($games as $value) {
+
+            /**
+             * les variables recupères et stock les valeurs du tableau games.
+             */
+            $name = $value['name'];
+            $price = $value['price'];
+        ?>
+            <tr>
+                <td><a href="5-superglobales.php?price=<?php echo $price; ?>">
+                        <?php echo $name ?></a></td>
+                <td><?php echo number_format($price, 2, ',', ' ');
+                    echo "€";
+                    if ($price < 50) {
+                        echo " pas chère !";
+                    }
+                    ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+</table>
