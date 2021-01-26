3aceb930c6e4d7052bc6fcbf4f3a94270693e210
Alice1360 <72809385+Alice1360@users.noreply.github.com>
2021-01-26 14:19:28 +0100

    exo5-Alice

--- 5-superglobales.php
+++ 5-superglobales.php
@@ -1,13 +1,33 @@
 <?php
+
+$price = $_GET['price'];
+
+if (!empty($price)) {
+    $_SESSION['price'] = $price;
+}
+if (isset($_POST['email'])) {
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        echo 'Veuillez entrer un email valide';
+    }
+}
+if (isset($_POST['email']) && isset($_POST['gift'])) {
+    $_SESSION['sum'] = $price + $_POST['gift'];
+    $_SESSION['email'] = $_POST['email'];
+    var_dump($_SESSION);
+}
+ 
+
 ?>
 
 <form action="" method="post">
     <div>
         <label for="gift">Ajouter un don</label>
-        <input type="number" id="gift">
+        <input type="number" id="gift" name="gift">
     </div>
     <div>
         <label for="email">Un champ email</label>
-        <input type="email" id="email">
+        <input type="email" id="email" name="email">
     </div>
+    <button type="submit">Valider</button>
 </form>
+
