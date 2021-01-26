156bf1c1655e95f3800d8c10dbe477b76ccddcc0
Adam FARES <dadi71410@outlook.fr>
2021-01-26 14:50:57 +0100

    exercice-3-terminé-Adam

--- 3-fonctions.php
+++ 3-fonctions.php
@@ -1,1 +1,20 @@
 <?php
+
+function fonctionAffine(int $n)
+{
+    if ($n>5) { 
+        return 4 * $n + 6;
+    } else {
+        return 3 * $n - 2;
+    }
+}
+
+
+function suiteNPlusUn(int $n): int
+{
+    return $n * ($n + 1)/2;
+};
+
+var_dump(fonctionAffine(3),fonctionAffine(2),fonctionAffine(4));
+
+var_dump(suiteNPlusUn(3),suiteNPlusUn(2),suiteNPlusUn(4));
